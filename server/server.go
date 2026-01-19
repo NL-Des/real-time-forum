180d30ba6b6fd4757b15c7e0a7229f854d58bd25
nleonard <nathan33.travail@gmail.com>
2026-01-19 14:56:16 +0100

    Début serveur et première page.

---
+++ server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func Server(port string) { // Première lettre en majuscule pour que la fonction soit publique.
+
+	// Définition de la route de l'unique page.
+	mainPage := func(w http.ResponseWriter, r *http.Request) {
+		// Lecture du fichier HTML
+		tmpl, err := template.ParseFiles("../frontend/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError) // Affiche sur le site.
+			fmt.Printf("Error Parsing HTML Page: %v", err)             // Affiche sur la console.
+		}
+		// Envoi le fichier HTML au w http.ResponseWriter.
+		// Ce qui l'envoie et l'affiche sur le site.
+		tmpl.Execute(w, nil)
+	}
+
+	// Si quelqu'un va sur mainPage, affiche mainPage
+	http.HandleFunc("/mainPage", mainPage)
+
+	// Lancement serveur Go
+	fmt.Printf("Serveur démarré sur le port %s", port)
+	err := http.ListenAndServe(port, nil) // A laisser à la fin, élément bloquant la lecture.
+	if err != nil {
+		fmt.Printf("Error lauching servor : %v", err)
+	}
+}
