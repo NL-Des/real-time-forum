b09053675e619c90e85ce4e0ce3702b5abca841e
Valentine <valentineladjyn@hotmail.fr>
2026-01-26 22:23:46 +0100

    définition de post et gestion d'erreur

--- posts/domain.go
+++ posts/domain.go
@@ -1,1 +1,8 @@
 package posts
+
+type Post struct {
+	ID           int
+	Title        string
+	Content      string
+	CategoriesId []int
+}
--- posts/service.go
+++ posts/service.go
@@ -1,1 +1,25 @@
 package posts
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func IsValid(post Post) error {
+	if post.Title == "" {
+		return fmt.Errorf("empty title")
+	} else if post.Content == "" {
+		return fmt.Errorf("no content provided")
+	}
+
+	return nil
+}
+
+func RespondError(w http.ResponseWriter, status int, err error) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(w).Encode(map[string]string{
+		"error": err.Error(),
+	})
+}
