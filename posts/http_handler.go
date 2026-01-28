cd2f6d52f5b9e451ab2093baea98e45e48e6f18c
Valentine <valentineladjyn@hotmail.fr>
2026-01-28 14:43:47 +0100

    initialisation handler

--- posts/http_handler.go
+++ posts/http_handler.go
@@ -1,1 +1,43 @@
 package posts
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func NewPostHandler(w http.ResponseWriter, r *http.Request) {
+	var newPost Post
+	// referer := r.Referer()
+	// if referer == "" {
+	// 	referer = "/"
+	// }
+
+	// -- récupération données --
+	if r.Method == "POST" {
+		if err := json.NewDecoder(r.Body).Decode(&newPost); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	// -- gestion d'erreurs --
+
+	if err := IsValid(newPost); err != nil {
+		log.Println("<NewPostHandler> Error invalid post: ", err)
+		RespondError(w, http.StatusBadRequest, err)
+	}
+
+	//sauvegarde db
+
+	//notif post ajouté
+}
+
+// Fonction renvoi message d'erreur dans le front
+func RespondError(w http.ResponseWriter, status int, err error) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(w).Encode(map[string]string{
+		"error": err.Error(),
+	})
+}
