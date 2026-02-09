76487fafe148c61d22c8261c854abf68e97a1278
Valentine <valentineladjyn@hotmail.fr>
2026-02-09 12:53:33 +0100

    handler fonctionnel gestion d'un nouveau comment

--- comments/http_handler.go
+++ comments/http_handler.go
@@ -1,1 +1,54 @@
 package comments
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"real-time-forum/shared"
+)
+
+func NewCommentHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//récup data
+
+		var newComment Comment
+
+		if r.Method == "POST" {
+			if err := json.NewDecoder(r.Body).Decode(&newComment); err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+
+		log.Println(newComment)
+
+		//check data
+		if err := IsValidFormat(newComment); err != nil {
+			log.Println("<NewCommentHandler> Error invalid comment: ", err)
+			shared.RespondError(w, http.StatusBadRequest, err)
+			return
+		}
+
+		if err := ValidateCommentReferences(db, &newComment); err != nil {
+			log.Println("<NewCommentHandler> Error invalid comment references: ", err)
+			shared.RespondError(w, http.StatusBadRequest, err)
+			return
+		}
+
+		//save data
+		if err := SaveComment(db, &newComment); err != nil {
+			log.Println("<NewCommentHandler> Error cannot save comment: ", err)
+			shared.RespondError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		// --- notification comment ajouté ---
+		log.Println("New comment saved into db")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "comment created",
+		})
+	}
+}
