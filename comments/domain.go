78064f8e01a74346b95376d22c435e90ed9b6e81
Valentine <valentineladjyn@hotmail.fr>
2026-02-09 12:51:52 +0100

    struct tag pour lecture json

--- comments/domain.go
+++ comments/domain.go
@@ -4,9 +4,9 @@ import "time"
 
 type Comment struct {
 	ID        int
-	PostID    int
-	AuthorID  int
-	Content   string
+	PostID    int    `json:"postid"`
+	AuthorID  int    `json:"authorid"`
+	Content   string `json:"content"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
