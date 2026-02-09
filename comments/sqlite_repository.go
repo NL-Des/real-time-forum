832cd4e5d8a31ad64da88c02db73e685de161e5a
Valentine <valentineladjyn@hotmail.fr>
2026-02-09 12:54:45 +0100

    fonctions sauvegarde et vérification nouveau comment

--- comments/sqlite_repository.go
+++ comments/sqlite_repository.go
@@ -1,1 +1,47 @@
 package comments
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+//IsValidMeta : exist post/author
+
+func SaveComment(db *sql.DB, comment *Comment) error {
+	query := `INSERT INTO comments (postid, authorid, content) VALUES (?, ?, ?)`
+	_, err := db.Exec(query, comment.PostID, comment.AuthorID, comment.Content)
+	return err
+}
+
+// --- Validité metadata ---
+
+func ValidateCommentReferences(db *sql.DB, comment *Comment) error {
+	var postID int
+	err := db.QueryRow(
+		`SELECT id FROM post WHERE id = ?`,
+		comment.PostID,
+	).Scan(&postID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return fmt.Errorf("post %d does not exist", comment.PostID)
+		}
+		return err
+	}
+
+	// Vérifier l’auteur
+	var authorID int
+	err = db.QueryRow(
+		`SELECT id FROM users WHERE id = ?`,
+		comment.AuthorID,
+	).Scan(&authorID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return fmt.Errorf("author %d does not exist", comment.AuthorID)
+		}
+		return err
+	}
+
+	return nil
+}
